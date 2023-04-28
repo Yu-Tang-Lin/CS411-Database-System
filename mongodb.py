5bf4ee0a3183787d34d0690e5b8cc680ec74b689
zhanchaoy417 <59937972+zhanchaoy417@users.noreply.github.com>
2023-04-28 17:38:15 -0600

    mogodb-4-29

--- mongodb.py
+++ mongodb.py
@@ -8,12 +8,21 @@ myclient = MongoClient("mongodb://localhost:27017/")
 db = myclient["academicworld"]
 
 # Created or Switched to collection
-Collection = db["faculty"]
+Faculty = db["faculty"]
 
-Collection = db["publications"]
+Publications = db["publications"]
 
 
 
 # Query
-count = db.faculty.count_documents({"position": "Assistant Professor"})
-print(count)
+#count = db.faculty.count_documents({"position": "Assistant Professor"})
+count = db.faculty.aggregate([
+    {"$match": {"affiliation.name": "University of illinois at Urbana Champaign"}},
+    {"$lookup": {"from": "publications", "localField": "publications", "foreignField": "id", "as": "temp"}},
+    {"$unwind": "$temp"},
+    { "$match":{ "temp.year": {"$gte":2020}}},
+    {"$group": {"_id": "$affiliation.name", "pub_cnt": {"$sum": 1}}}
+])
+
+for result in count:
+    print(result['pub_cnt'])
