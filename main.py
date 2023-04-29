845223e675872e81c0eaa031fbd5eabe6ac6a47e
zhanchaoy417 <59937972+zhanchaoy417@users.noreply.github.com>
2023-04-28 23:14:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,136 +1,174 @@
 import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import dash
 from dash import Dash, html, dcc, callback, Output, Input, dash_table, State
-#from neo4j import GraphDatabase
 
-#from mysql_utils import get_university
+#from neo4j_u import GraphDatabase
+#from mongodb import publication_c
 
-
-import pandas as pd
-# neo_db = db = GraphDatabase.driver('bolt://127.0.0.1:7687')
-
-app = Dash(__name__)
-
-app.layout =  html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Input(id='input'),
-    html.Button('Search', id='search_button'),
-    dash_table.DataTable(
-        columns = [{'name': 'University Name', 'id': 'name'}, {'name': 'University Id', 'id': 'id'}],
-        id='university_table'),
-    dcc.Textarea(id='tid'),
+from mysql_utils import get_university, faculty_count, keyword_count, publication_count, publication_count_year
 
 
-    dcc.Input(
-        id='num-multi',
-        type='number',
-        value=5
-    ),
-    html.Table([
-        html.Tr([html.Td(['x', html.Sup(2)]), html.Td(id='square')]),
-        html.Tr([html.Td(['x', html.Sup(3)]), html.Td(id='cube')]),
-        html.Tr([html.Td([2, html.Sup('x')]), html.Td(id='twos')]),
-        html.Tr([html.Td([3, html.Sup('x')]), html.Td(id='threes')]),
-        html.Tr([html.Td(['x', html.Sup('x')]), html.Td(id='x^x')]),
-    ]),
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-    html.Div([
+# Define the layout
+app.layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("Widget 1"),
+                                dbc.CardBody(
+                                    [
+                                        dcc.Input(id='input'),
+                                        html.Button('Search', id='search_button'),
+                                        # faculty_count
+                                        dash_table.DataTable(
+                                            columns=[{'name': 'University Name', 'id': 'name'},
+                                                     {'name': 'Publication_count', 'id': 'count1'}],
+                                            id='faculty_count_table',
+                                        ),
 
-        html.Div([
-            dcc.Dropdown(
-                'Fertility rate, total (births per woman)',
-                id='crossfilter-xaxis-column',
-            ),
-            dcc.RadioItems(
-                ['Linear', 'Log'],
-                'Linear',
-                id='crossfilter-xaxis-type',
-                labelStyle={'display': 'inline-block', 'marginTop': '5px'}
-            )
-        ],
-            style={'width': '49%', 'display': 'inline-block'}),
 
-        html.Div([
-            dcc.Dropdown(
-                'Life expectancy at birth, total (years)',
-                id='crossfilter-yaxis-column'
-            ),
-            dcc.RadioItems(
-                ['Linear', 'Log'],
-                'Linear',
-                id='crossfilter-yaxis-type',
-                labelStyle={'display': 'inline-block', 'marginTop': '5px'}
-            )
-        ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
-    ], style={
-        'padding': '10px 5px'
-    }),
+                                        dcc.Input(id="widget-1-input", type="text", value="Widget 1"),
+                                        html.Br(),
+                                        dbc.Button("Submit", id="widget-1-button", color="primary"),
+                                        dcc.Textarea(id='tid'),
+                                    ]
+                                ),
+                            ],
+                            className="mt-3",
+                        ),
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("Widget 2"),
+                                dbc.CardBody(
+                                    [
+                                        dcc.Input(id='input2'),
+                                        html.Button('Search', id='search_button2'),
+                                        dash_table.DataTable(
+                                            columns=[{'name': 'University Name', 'id': 'name'},
+                                                     {'name': 'Publication_count', 'id': 'count3'}],
+                                            id='publication_count_table',
+                                        ),
 
-    html.Div([
-        dcc.Graph(
-            id='crossfilter-indicator-scatter',
-            hoverData={'points': [{'customdata': 'Japan'}]}
+                                        dcc.Textarea(id='tid2'),
+                                        dcc.Input(id="widget-2-input", type="text", value="Widget 2"),
+                                        html.Br(),
+                                        dbc.Button("Submit", id="widget-2-button", color="primary"),
+                                    ]
+                                ),
+                            ],
+                            className="mt-3",
+                        ),
+                    ],
+                    width=4,
+                ),
+                dbc.Col(
+                    [
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("Widget 3"),
+                                dbc.CardBody(
+                                    [
+                                        dcc.Input(id="widget-3-input", type="text", value="Widget 3"),
+                                        html.Br(),
+                                        dbc.Button("Submit", id="widget-3-button", color="primary"),
+                                    ]
+                                ),
+                            ],
+                            className="mt-3",
+                        ),
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("Widget 4"),
+                                dbc.CardBody(
+                                    [
+                                        dcc.Input(id="widget-4-input", type="text", value="Widget 4"),
+                                        html.Br(),
+                                        dbc.Button("Submit", id="widget-4-button", color="primary"),
+                                    ]
+                                ),
+                            ],
+                            className="mt-3",
+                        ),
+                    ],
+                    width=4,
+                ),
+                dbc.Col(
+                    [
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("Widget 5"),
+                                dbc.CardBody(
+                                    [
+                                        dcc.Input(id="widget-5-input", type="text", value="Widget 5"),
+                                        html.Br(),
+                                        dbc.Button("Submit", id="widget-5-button", color="primary"),
+                                    ]
+                                ),
+                            ],
+                            className="mt-3",
+                        ),
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("Widget 6"),
+                                dbc.CardBody(
+                                    [
+                                        dcc.Input(id="widget-6-input", type="text", value="Widget 6"),
+                                        html.Br(),
+                                        dbc.Button("Submit", id="widget-6-button", color="primary"),
+                                    ]
+                                ),
+                            ],
+                            className="mt-3",
+                        ),
+                    ],
+                    width=4,
+                ),
+            ]
         )
-    ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
-    html.Div([
-        dcc.Graph(id='x-time-series'),
-        dcc.Graph(id='y-time-series'),
-    ], style={'display': 'inline-block', 'width': '49%'}),
-
-
-
-])
-
-
-
-
-@app.callback(
-    Output('square', 'children'),
-    Output('cube', 'children'),
-    Output('twos', 'children'),
-    Output('threes', 'children'),
-    Output('x^x', 'children'),
-    Input('num-multi', 'value'))
-def callback_a(x):
-    return x**2, x**3, 2**x, 3**x, x**x
-def update_graph(xaxis_column_name, yaxis_column_name,
-                 xaxis_type, yaxis_type,
-                 year_value):
-    dff = df[df['Year'] == year_value]
-
-    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
-            y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
-            hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name']
-            )
-
-    fig.update_traces(customdata=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'])
-
-    fig.update_xaxes(title=xaxis_column_name, type='linear' if xaxis_type == 'Linear' else 'log')
-
-    fig.update_yaxes(title=yaxis_column_name, type='linear' if yaxis_type == 'Linear' else 'log')
-
-    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
-
-    return fig
-
-@callback(
-    Output('university_table', 'data'),
-    Output('tid', 'value'),
-    State('input', 'value'),
-    Input('search_button', 'n_clicks')
+    ],
+    fluid=True,
 )
+@callback(
+    Output('faculty_count_table', 'data'),
+    Output('tid', 'value'),
+
+    State('input', 'value'),
+    Input('search_button', 'n_clicks'),
+
+)
+
+
 def update_table(input_value, n_clicks):
     if not input_value:
         return dash.no_update
-    result = get_university(input_value)
+    #result = get_university(input_value)
+    result = faculty_count(input_value)
     print(result)
-    return result, 'search result for :' + input_value
+    return result['pub_cnt'], 'search result for :' + input_value
 
-if __name__ == '__main__':
-    app.run_server()
+@callback(
+    Output('publication_count_table', 'data'),
+    Output('tid2', 'value'),
 
+    State('input2', 'value'),
+    Input('search_button2', 'n_clicks'),
 
+)
+def update_table2(input_value, n_clicks):
+    if not input_value:
+        return dash.no_update
+    #result = get_university(input_value)
+    result = publication_count(input_value)
+    print(result)
+    return result, 'search2 result for :' + input_value
 
-
-
-
-
+if __name__ == "__main__":
+    app.run_server(debug=True)
