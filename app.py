e35945a5e8aa9944035a0e2a4245b82ce6588322
zhanchaoy417 <59937972+zhanchaoy417@users.noreply.github.com>
2023-04-28 23:21:59 -0600

    main.py-1:21

--- app.py
+++ app.py
@@ -1,73 +1,137 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
 import dash
 from dash import Dash, html, dcc, callback, Output, Input, dash_table, State
 
-#from neo4j import GraphDatabase
+#from neo4j_u import GraphDatabase
+#from mongodb import publication_c
 
-from mysql_utils import db,get_university, faculty_count, keyword_count, publication_count, publication_count_year
-
-import pandas as pd
-import plotly.express as px
-
-# Incorporate data
-#df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
-#neo_db = db = GraphDatabase.driver('bolt://127.0.0.1:7687')
-
-app = Dash(__name__)
-
-import plotly.graph_objs as go
+from mysql_utils import get_university, faculty_count, keyword_count, publication_count, publication_count_year
 
 
-app.layout = html.Div([
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Input(id='input'),
-    html.Button('Search', id='search_button'),
-    #faculty_count
-    dash_table.DataTable(
-        columns =[{'name': 'University Name', 'id': 'name'}, {'name': 'Publication_count', 'id': 'count1'}],
-        id='faculty_count_table',
-        ),
+# Define the layout
+app.layout = html.Div([   html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+        dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("Widget 1"),
+                                dbc.CardBody(
+                                    [
+                                        dcc.Input(id='input'),
+                                        html.Button('Search', id='search_button'),
+                                        # faculty_count
+                                        dash_table.DataTable(
+                                            columns=[{'name': 'University Name', 'id': 'name'},
+                                                     {'name': 'faculty_count', 'id': 'count1'}],
+                                            id='faculty_count_table',
+                                        ),
+                                        dcc.Textarea(id='tid'),
+                                    ]
+                                ),
+                            ],
+                            className="mt-3",
+                        ),
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("Widget 2"),
+                                dbc.CardBody(
+                                    [
+                                        dcc.Input(id='input2'),
+                                        html.Button('Search', id='search_button2'),
+                                        dash_table.DataTable(
+                                            columns=[{'name': 'University Name', 'id': 'name'},
+                                                     {'name': 'Publication_count', 'id': 'count3'}],
+                                            id='publication_count_table',
+                                        ),
 
-dcc.Graph(
-    figure={
-        'data': [
-            {'x': ['f', 'a', 'c'], 'y': [1,2,3], 'type': 'bar', 'name': 'SF'},
-            {'x': ['f', 'a', 'c'], 'y': [1,2,3], 'type': 'bar', 'name': u'Montréal'},
-        ],
-        'layout': {
-            'title': 'Dash Data Visualization'
-        }
-    }
-),
- dcc.Graph(
-        id='plot',
-        style={"max-width": "600px",
-               "margin": "auto",
-               "display": "inline-block"})
-,
-
-    #dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg')),
-    #dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg')),
-
-    dcc.Textarea(id='tid'),
-
-    dcc.Input(id='input2'),
-    html.Button('Search', id='search_button2'),
-    dash_table.DataTable(
-        columns=[{'name': 'University Name', 'id': 'name'}, {'name': 'Publication_count', 'id': 'count3'}],
-        id='publication_count_table',
-    ),
-
-    dcc.Textarea(id='tid2')
-
-])
-
+                                        dcc.Textarea(id='tid2'),
+                                        dcc.Input(id="widget-2-input", type="text", value="Widget 2"),
+                                        html.Br(),
+                                        dbc.Button("Submit", id="widget-2-button", color="primary"),
+                                    ]
+                                ),
+                            ],
+                            className="mt-3",
+                        ),
+                    ],
+                    width=4,
+                ),
+                dbc.Col(
+                    [
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("Widget 3"),
+                                dbc.CardBody(
+                                    [
+                                        dcc.Input(id="widget-3-input", type="text", value="Widget 3"),
+                                        html.Br(),
+                                        dbc.Button("Submit", id="widget-3-button", color="primary"),
+                                    ]
+                                ),
+                            ],
+                            className="mt-3",
+                        ),
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("Widget 4"),
+                                dbc.CardBody(
+                                    [
+                                        dcc.Input(id="widget-4-input", type="text", value="Widget 4"),
+                                        html.Br(),
+                                        dbc.Button("Submit", id="widget-4-button", color="primary"),
+                                    ]
+                                ),
+                            ],
+                            className="mt-3",
+                        ),
+                    ],
+                    width=4,
+                ),
+                dbc.Col(
+                    [
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("Widget 5"),
+                                dbc.CardBody(
+                                    [
+                                        dcc.Input(id="widget-5-input", type="text", value="Widget 5"),
+                                        html.Br(),
+                                        dbc.Button("Submit", id="widget-5-button", color="primary"),
+                                    ]
+                                ),
+                            ],
+                            className="mt-3",
+                        ),
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("Widget 6"),
+                                dbc.CardBody(
+                                    [
+                                        dcc.Input(id="widget-6-input", type="text", value="Widget 6"),
+                                        html.Br(),
+                                        dbc.Button("Submit", id="widget-6-button", color="primary"),
+                                    ]
+                                ),
+                            ],
+                            className="mt-3",
+                        ),
+                    ],
+                    width=4,
+                ),
+            ]
+        )
+    ],
+    fluid=True,
+) ])
 @callback(
     Output('faculty_count_table', 'data'),
     Output('tid', 'value'),
@@ -101,24 +165,6 @@ def update_table2(input_value, n_clicks):
     result = publication_count(input_value)
     print(result)
     return result, 'search2 result for :' + input_value
-@callback(
-    Output('plot', 'figure'),
-    Input('publication_count_table', 'columns')
-)
-def update_graph(cols):
-    df = pd.DataFrame( columns=[c['University Name'] for c in cols])
-    figure_2 = go.Figure(data=[
-        go.Bar(x=df[''],
-               y=df['Rate'],
-               width=0.45,
-               text=df['Rate'],
-               textposition='inside',
-               marker_color='indianred')])
-    return figure_2
-if __name__ == '__main__':
-    app.run_server()
 
-
-
-
-
+if __name__ == "__main__":
+    app.run_server(debug=True)
