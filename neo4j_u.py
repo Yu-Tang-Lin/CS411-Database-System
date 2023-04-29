aef1b7d11af662e4ee6ae4eee2a78a78b8971ce6
zhanchaoy417 <59937972+zhanchaoy417@users.noreply.github.com>
2023-04-29 14:23:39 -0600

    4-29

--- neo4j_u.py
+++ neo4j_u.py
@@ -1,16 +1,20 @@
 uri = "bolt://localhost:7687"
 username = "neo4j"
 password = "cs411cs411"
-
+from neo4j import GraphDatabase
 driver = GraphDatabase.driver(uri, auth=(username, password))
+university = "University of illinois at Urbana Champaign"
 
 
-# Create a session
-with driver.session(database='academicworld') as session:
-    # Execute a read transaction
-    result = session.run('MATCH (k:KEYWORD)-[:INTERESTED_IN]-(f:FACULTY)-[:AFFILIATION_WITH]-(I:INSTITUTE) WHERE I.name = "University of illinois at Urbana Champaign" RETURN count(distinct k) AS keyword_count')
-    for record in result:
-        print(record)
-
+def keyword_count(university):
+    # Create a session
+    with driver.session(database='academicworld') as session:
+        # Execute a read transaction
+        result = session.run('MATCH (k:KEYWORD)-[:INTERESTED_IN]-(f:FACULTY)-[:AFFILIATION_WITH]-(I:INSTITUTE) '
+                             'WHERE I.name = "' + str(university) + '" RETURN count(distinct k) AS keyword_count')
+        for record in result:
+            print(record)
+    return result
+count = keyword_count(university)
 # Close the driver
 driver.close()
