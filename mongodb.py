2fbaa372460334e02783e4313a8480e68b699d62
zhanchaoy417 <59937972+zhanchaoy417@users.noreply.github.com>
2023-04-29 14:23:04 -0600

    4-29

--- mongodb.py
+++ mongodb.py
@@ -16,7 +16,8 @@ Publications = db["publications"]
 
 # Query
 #count = db.faculty.count_documents({"position": "Assistant Professor"})
-count = db.faculty.aggregate([
+def publication_c():
+    count = db.faculty.aggregate([
     {"$match": {"affiliation.name": "University of illinois at Urbana Champaign"}},
     {"$lookup": {"from": "publications", "localField": "publications", "foreignField": "id", "as": "temp"}},
     {"$unwind": "$temp"},
@@ -24,5 +25,29 @@ count = db.faculty.aggregate([
     {"$group": {"_id": "$affiliation.name", "pub_cnt": {"$sum": 1}}}
 ])
 
-for result in count:
-    print(result['pub_cnt'])
+    for result in count:
+        print(result['pub_cnt'])
+    return result['pub_cnt']
+#publication_c()
+#print(result['pub_cnt'])
+
+university = "University of illinois at Urbana Champaign"
+
+def publication_count_by_year(university):
+    # Query
+
+    university = "University of illinois at Urbana Champaign"
+    #count = db.faculty.count_documents({"position": "Assistant Professor"})
+    count = db.faculty.aggregate([
+        {"$match": {"affiliation.name": university}},
+        {"$lookup": {"from": "publications", "localField": "publications", "foreignField": "id", "as": "temp"}},
+        {"$unwind": "$temp"},
+        { "$match":{ "temp.year": {"$gte":2020}}},
+        {"$group": {"_id": "$affiliation.name", "pub_cnt": {"$sum": 1}}}
+    ])
+
+    for result in count:
+        print(result['pub_cnt'])
+    return result
+count = publication_count_by_year(university)
+print(count)
