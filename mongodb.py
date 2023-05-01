6ac8c6015c80fc0561caac788a4a568d4b6f29f4
zhanchaoy417 <59937972+zhanchaoy417@users.noreply.github.com>
2023-04-30 19:40:16 -0600

    update-5/1

--- mongodb.py
+++ mongodb.py
@@ -1,6 +1,6 @@
 import json
 from pymongo import MongoClient
-
+import pandas as pd
 # Making Connection
 myclient = MongoClient("mongodb://localhost:27017/")
 
@@ -15,23 +15,23 @@ Publications = db["publications"]
 
 
 
-university = "University of illinois at Urbana Champaign"
+#university = "santa"
 
 def publication_count_by_year(university):
     # Query
-
-    university = "University of illinois at Urbana Champaign"
-    #count = db.faculty.count_documents({"position": "Assistant Professor"})
+    university_query = { "$regex": university, "$options": "i" }
+    print("start query")
     count = db.faculty.aggregate([
-        {"$match": {"affiliation.name": university}},
+        {"$match": {"affiliation.name": university_query}},
         {"$lookup": {"from": "publications", "localField": "publications", "foreignField": "id", "as": "temp"}},
         {"$unwind": "$temp"},
-        { "$match":{ "temp.year": {"$gte":2020}}},
+        {"$match":{ "temp.year": {"$gte":2020}}},
         {"$group": {"_id": "$affiliation.name", "pub_cnt": {"$sum": 1}}}
     ])
-
-    for result in count:
-        print(result['pub_cnt'])
-    return result
-count = publication_count_by_year(university)
-print(count)
+    df = pd.DataFrame(columns=["_id", "pub_cnt"])
+    for record in count:
+        df = df.append(record, ignore_index=True)
+    print(df)
+    return df
+#count = publication_count_by_year(university)
+#print(count)
