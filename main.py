66dcea10ee63286ac3c7e943f1027c2dc3bdc532
zhanchaoy417 <59937972+zhanchaoy417@users.noreply.github.com>
2023-04-30 19:38:43 -0600

    update-5/1

--- main.py
+++ main.py
@@ -4,19 +4,18 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 import dash
 from dash import Dash, html, dcc, callback, Output, Input, dash_table, State
-
-#from neo4j_u import GraphDatabase
-from mongodb import publication_c
+import pandas as pd
+from neo4j_u import GraphDatabase
+from mongodb_utils import publication_count_by_year
 import plotly.graph_objs as go
+from mysql_utils import faculty_count, publication_count,insert_keyword,delete_keyword
 import pandas as pd
 import plotly.express as px
-from mysql_utils import  insert_keyword,delete_keyword,faculty_count, keyword_count, publication_count, publication_count_year
-
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Define the layout
-app.layout = html.Div([   html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+app.layout = html.Div([   html.H1(children='Acadamic world dashboard', style={'textAlign':'center'}),
         dbc.Container(
     [
         dbc.Row(
@@ -69,8 +68,9 @@ app.layout = html.Div([   html.H1(children='Title of Dash App', style={'textAlig
                                     [
                                         dcc.Input(id='input3', type='text', placeholder='Enter first string'),
                                         dcc.Input(id='input3-2', type='text', placeholder='Enter second string'),
+                                        html.Button('Submit', id='widget3-submit-button', n_clicks=0),
+
                                         html.Div(id='output'),
-                                        dbc.Button("Submit", id="widget-3-button", color="primary"),
 
 
                                         # faculty_count
@@ -94,8 +94,8 @@ app.layout = html.Div([   html.H1(children='Title of Dash App', style={'textAlig
                                     [
 
                                         dcc.Input(id='input4', type='text', placeholder='Enter second string'),
+                                        html.Button('Submit', id='widget4-submit-button', n_clicks=0),
                                         html.Div(id='output2'),
-                                        dbc.Button("Submit", id="widget-4-button", color="primary"),
 
                                     ]
                                 ),
@@ -112,11 +112,15 @@ app.layout = html.Div([   html.H1(children='Title of Dash App', style={'textAlig
                                 dbc.CardHeader("Widget 5"),
                                 dbc.CardBody(
                                     [
-                                        dcc.Input(id="widget-5-input", type="text", value="Widget 5"),
-                                        html.Br(),
-                                        dbc.Button("Submit", id="widget-5-button", color="primary"),
+                                        dcc.Input(id='input5'),
+                                        html.Button('Search', id='search_button5'),
                                     ]
                                 ),
+                                dcc.Graph(
+                                    id='histogram5',
+                                    figure={
+                                    }
+                                )
                             ],
                             className="mt-3",
                         ),
@@ -124,9 +128,11 @@ app.layout = html.Div([   html.H1(children='Title of Dash App', style={'textAlig
                             [
                                 dbc.CardHeader("Widget 6"),
                                 dbc.CardBody(
-                                    [
+                                    [   dcc.Input(id='input6'),
+                                        html.Button('Search', id='search_button6'),
                                         dcc.Input(id="widget-6-input", type="text", value="Widget 6"),
                                         html.Br(),
+                                        html.Div(id='output6'),
                                         dbc.Button("Submit", id="widget-6-button", color="primary"),
 
                                     ]
@@ -198,22 +204,63 @@ def update_table2(input_value, n_clicks):
 
 @callback(
     dash.dependencies.Output('output', 'children'),
+    [dash.dependencies.Input('widget3-submit-button', 'n_clicks')],
     [dash.dependencies.Input('input3', 'value'),
      dash.dependencies.Input('input3-2', 'value')])
-def update_output(str1, str2):
+def update_output(n_clicks,str1, str2):
+    if n_clicks > 0 and str1 is not None and str2 is not None:
         result = insert_keyword(str1,str2)
-        return f"insert keyword to database: {str2},{result}"
+        return html.Div([
+            html.P(f"insert keyword to database: {str2}"),
+            html.P(f"{result}")
+        ])
+        #return f"insert keyword to database: {str2},{result}"
 
 
 #widget4
 
 @callback(
     dash.dependencies.Output('output2', 'children'),
+    [dash.dependencies.Input('widget4-submit-button', 'n_clicks')],
     [dash.dependencies.Input('input4', 'value')])
 
-def update_output(str3):
+def update_output(n_clicks,str3):
+    if n_clicks > 0 and str3 is not None:
         result = delete_keyword(str3)
-        return f"delete keyword to database: {str3},{result}"
+        return html.Div([
+            html.P(f"delete keyword to database: {str3}"),
+            html.P(f"{result}")
+        ])
+        #return f"delete keyword to database: {str3},{result}"
+## wignet5
+@callback(
+    Output('histogram5', 'figure'),
+    State('input5', 'value'),
+    Input('search_button5', 'n_clicks'),
+)
+
+def update_table5(input_value, n_clicks):
+    if not input_value:
+        return dash.no_update
+    result = publication_count_by_year(input_value)
+    print(result)
+    df = result
+    num_rows = len(df)
+    return {
+        'data': [
+            go.Treemap(
+                labels=df["_id"],
+                parents=[""] * num_rows,  # since there are no subcategories for this treemap, all parents will be blank
+                values=df["pub_cnt"],
+            )
+        ],
+        'layout': go.Layout(
+            title='Publication Count for recent three year',
+            xaxis={'title': 'University'},
+            yaxis={'title': 'Publication Count'}
+        )
+    }
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
